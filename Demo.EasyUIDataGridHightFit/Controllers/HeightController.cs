24fd70025fad1e4ccff878520dfb27ac93eb87dd
zeje <zezhi821@163.com>
2015-06-18 08:40:01 +0800

    First
    First

---
+++ Demo.EasyUIDataGridHightFit/Controllers/HeightController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.EasyUIDataGridHightFit.Controllers
+{
+    public class HeightController : Controller
+    {
+        //
+        // GET: /Height/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DG()
+        {
+            return View();
+        }
+
+    }
+}
